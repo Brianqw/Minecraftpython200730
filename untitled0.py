c0136ec7b1660ba2596f932822d5748e47a0603d
Brianqw <68846390+Brianqw@users.noreply.github.com>
2020-07-30 14:25:01 +0800

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 09:04:44 2020
+
+@author: user
+"""
+
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+number=1
+x,y,z = mc.player.getPos()
+for i in range(8):
+    for j in range(number):
+        mc.spawnEntity(x,y,z,60)
+    number = number * 2
+    mc.postToChat(str(number))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
